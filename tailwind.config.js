055a46a37a311b9c5948ef2d36a4a4692cce9250
nixonvitson <surfbreakcamp@gmail.com>
2025-05-14 00:43:29 +0530

    delete russian words

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Указываем папки, в которых используется Tailwind
     "./app/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Montserrat', 'sans-serif'], // Добавляем Montserrat в шрифт по умолчанию
+        sans: ['Montserrat', 'sans-serif'],
       },
     },
   },
